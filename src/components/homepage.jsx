b91856ca114d331e9d678e135432fe8426e8c5b7
Victor Ramirez <vicdeveloper83@gmail.com>
2022-07-03 19:01:10 +0100

    first commit

---
+++ src/components/homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {silenceDog} from '../assets/images'
+const Homepage=()=>{
+
+  return(
+    <>
+      <img src={silenceDog} alt="" />
+    </>
+)
+}
+
+export default Homepage
